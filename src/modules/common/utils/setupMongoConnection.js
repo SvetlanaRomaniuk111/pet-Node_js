0717744ef9e91f551cb3d3a22b84e25001a6511e
SvetlanaRomaniuk111 <gatskosv@gmail.com>
2023-12-01 08:18:43 +0200

    add basic mongo connection

--- src/modules/common/utils/setupMongoConnection.js
+++ src/modules/common/utils/setupMongoConnection.js
@@ -1,3 +1,36 @@
 const mongoose = require('mongoose');
+const {
+  DB_MONGO_USER,
+  DB_MONGO_PASSWORD,
+  DB_MONGO_HOST,
+  DB_MONGO_PORT,
+  DB_MONGO_DATABASE
+} = require('../constants/env');
 
-mongoose.connect('mongodb://username:password@host:port/database?options...');
+const setupMongoConnection = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${DB_MONGO_USER}:${DB_MONGO_PASSWORD}@${DB_MONGO_HOST}/${DB_MONGO_DATABASE}`,
+    );
+    console.log('Mongo connection is established!')
+  } catch (err) {
+    console.log(err);
+    process.exit(1)
+  }
+};
+
+module.exports = setupMongoConnection;
+
+// const setupMongoConnection = async () => {
+//   try {
+//     const status = await mongoose.connect(
+//       `mongodb+srv://${DB_MONGO_USER}:${DB_MONGO_PASSWORD}@${DB_MONGO_HOST}/${DB_MONGO_DATABASE}`,
+//     );
+//     console.log(status);
+//     console.log('Mongo connection is established!')
+//   } catch (err) {
+//     console.log(err);
+//     process.exit(1)
+//   }
+// };
+
